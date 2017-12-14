7cfb99f91b9a2b8a2685d169026a32c942321b47
kei ishihara <k.passtd@gmail.com>
2017-12-14 17:30:11 +0900

    group機能の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :members
+  has_many :messages
+  has_many :groups, through: :members
+
+  validates :name,
+    presence: true,
+    uniqueness: true
+  validates :email,
+    presence: true,
+    uniqueness: true
 end
