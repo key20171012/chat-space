c1a298580d82cca094d7f8871e35f102bc902472
kei ishihara <k.passtd@gmail.com>
2017-12-15 19:20:50 +0900

    add "send messages func"

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,11 @@
 class Message < ApplicationRecord
+  belongs_to :group
+  belongs_to :user
+  validates :content, presence: true, if: :messages_blank?
+  mount_uploader :image, ImageUploader
+
+
+  def messages_blank?
+    content == nil && image == nil
+  end
 end
