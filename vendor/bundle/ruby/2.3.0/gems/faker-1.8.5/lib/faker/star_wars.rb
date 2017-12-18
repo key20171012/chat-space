52b2fcc02c8e8021404446116a8b30bd257a5315
kei ishihara <k.passtd@gmail.com>
2017-12-18 20:57:09 +0900

    メッセージ送信機能の実装

---
+++ vendor/bundle/ruby/2.3.0/gems/faker-1.8.5/lib/faker/star_wars.rb
@@ -0,0 +1,82 @@
+module Faker
+  class StarWars < Base
+    class << self
+      def character
+        sample(characters)
+      end
+
+      def droid
+        sample(droids)
+      end
+
+      def planet
+        sample(planets)
+      end
+
+      def specie
+        sample(species)
+      end
+
+      def vehicle
+        sample(vehicles)
+      end
+
+      def wookiee_sentence
+        sentence = sample(wookiee_words).capitalize
+
+        rand(0..10).times { sentence += " " + sample(wookiee_words)}
+
+        sentence + sample(['.','?','!'])
+      end
+
+      def characters
+        fetch('star_wars.characters')
+      end
+
+      def droids
+        fetch('star_wars.droids')
+      end
+
+      def planets
+        fetch('star_wars.planets')
+      end
+
+      def quote(character = nil)
+        quoted_characters = translate('faker.star_wars.quotes')
+
+        if character.nil?
+          character = sample(quoted_characters.keys).to_s
+        else
+          character.to_s.downcase!
+
+          # check alternate spellings, nicknames, titles of characters
+          translate('faker.star_wars.alternate_character_spellings').each do |k, v|
+            character = k.to_s if v.include?(character)
+          end
+
+          unless quoted_characters.keys.include?(character.to_sym)
+            raise ArgumentError, "Character for quotes can be left blank or #{quoted_characters.keys.join(', ')}"
+          end
+        end
+
+        fetch('star_wars.quotes.' + character)
+      end
+
+      def species
+        fetch('star_wars.species')
+      end
+
+      def vehicles
+        fetch('star_wars.vehicles')
+      end
+
+      def wookiee_words
+        fetch('star_wars.wookiee_words')
+      end
+
+      alias_method :wookie_sentence, :wookiee_sentence
+      alias_method :wookie_words, :wookiee_words
+
+    end
+  end
+end
