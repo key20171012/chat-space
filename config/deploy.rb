4561737914d6ff65176044c184e7b9c5a3a12ab4
kei ishihara <k.passtd@gmail.com>
2017-12-28 04:07:07 +0900

    caistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,6 @@ lock "~> 3.10.1"
 
 set :application, "chat-space"
 set :repo_url,  'git@github.com:key20171012/chat-space.git'
-set :repo_url, "git@example.com:me/my_repo.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -11,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/kei/.ssh/keypair.pem']  
+                  keys: ['/Users/kei/.ssh/keypair.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
